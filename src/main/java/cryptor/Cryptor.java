45562baa4bf2d9136d62ba86abd2e4e868d847a5
rmk mac <anton.rymarenko@gmail.com>
2024-11-06 17:28:50 +0200

    first commit

---
+++ src/main/java/cryptor/Cryptor.java
@@ -0,0 +1,10 @@
+package cryptor;
+
+public class Cryptor {
+
+    public static void decrypt(){}
+
+    public static void encrypt(){}
+
+    public static void brutoForce(){}
+}
---
+++ src/main/java/services/FileWorker.java
@@ -0,0 +1,14 @@
+package services;
+
+import java.nio.file.Path;
+
+public class FileWorker {
+
+    public void fileReader(Path path){
+
+    }
+
+    public void fileWriter(String line){
+
+    }
+}
