61e7712cd6e567feda2332efc53cb56eb3a40a13
rmk mac <anton.rymarenko@gmail.com>
2024-11-15 13:00:02 +0200

    update main method and readmi file

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-This is a cypher for encrypting and decrypting text from file. Under the hood it uses Caesar crypting method 
+This is a cypher for encrypting and decrypting text from file. Under the hood it uses Caesar crypting method
+
+For encrypting and decrypting use format: 
+"java -jar cryptor-1.0-SNAPSHOT.jar ENCRYPT/DECRYPT 'filePath/filename' shift
+
+For bruteforce mode use format: 
+java -jar cryptor-1.0-SNAPSHOT.jar BRUTE_FORCE 'filePath/filename'
--- src/main/java/App.java
+++ src/main/java/App.java
@@ -13,9 +13,9 @@ public class App
         if (args.length < 2) {
             throw new WrongNumberArgumentsException("Please add at least 2 arguments\n" +
                     "For encrypting and decrypting use format: " +
-                    "java -jar cryptor-1.0-SNAPSHOT.jar ENCRYPT/DECRYPT filePath/filename shift\n" +
+                    "java -jar cryptor-1.0-SNAPSHOT.jar ENCRYPT/DECRYPT \'filePath/filename\' shift\n" +
                     "For bruteforce mode use format: " +
-                    "java -jar cryptor-1.0-SNAPSHOT.jar BRUTE_FORCE filePath/filename");}
+                    "java -jar cryptor-1.0-SNAPSHOT.jar BRUTE_FORCE \'filePath/filename\'");}
 
 
             Runner.run(args);
